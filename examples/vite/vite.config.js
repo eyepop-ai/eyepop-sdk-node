1b14f6251cf82dc803abbcdb14a2710bb675b917
Edmund Dao <61922101+64blit@users.noreply.github.com>
2024-06-20 15:05:04 -0700

    [create] vite config added to handle dynamic importing of ep.min.js

---
+++ examples/vite/vite.config.js
@@ -0,0 +1,14 @@
+import { defineConfig } from 'vite'
+import path from 'path'
+
+export default defineConfig({
+    server: {
+        port: 8000,
+    },
+    resolve: {
+        alias: {
+            'eyepop': path.resolve(__dirname, '../../src/eyepop/dist/eyepop.min.js'),
+            'eyepop-render-2d': path.resolve(__dirname, '../../src/eyepop-render-2d/dist/eyepop.render2d.min.js'),
+        }
+    },
+})
