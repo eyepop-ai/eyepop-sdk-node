793b9fed4418226ade19c95dd73b97b6e9c21922
Edmund Dao <61922101+64blit@users.noreply.github.com>
2024-06-24 11:18:32 -0700

    [update] added type export and better defaults

--- src/eyepop-render-2d/render-pose.ts
+++ src/eyepop-render-2d/render-pose.ts
@@ -3,7 +3,7 @@ import { Style } from "./style";
 import { PredictedKeyPoint, PredictedKeyPoints, PredictedObject, StreamTime } from "@eyepop.ai/eyepop";
 import { Render, DEFAULT_TARGET, RenderTarget } from './render';
 
-type RenderPoseOptions = {} & RenderTarget
+export type RenderPoseOptions = {} & RenderTarget
 
 export class RenderPose implements Render
 {
@@ -14,7 +14,7 @@ export class RenderPose implements Render
 
     constructor(options: Partial<RenderPoseOptions> = {})
     {
-        const { target = DEFAULT_TARGET } = options;
+        const { target = '$..objects[?(@.category=="person")]' } = options;
         this.target = target;
     }
 
