e0755b39224e29af4db1aaac6dd41ec2154a7957
Torsten Schulz <torsten@eyepop.ai>
2024-07-22 12:12:22 -0700

    [wip] unit tests for data endpoint

--- tests/eyepop/data/endpoint.connect.test.ts
+++ tests/eyepop/data/endpoint.connect.test.ts
@@ -1,19 +1,25 @@
 import {EyePop, Session} from '../../../src/eyepop'
 
 import {MockServer} from 'jest-mock-server'
+import {WS} from 'jest-websocket-mock'
 
 import {describe, expect, test} from '@jest/globals'
 import {v4 as uuidv4} from 'uuid'
 
-
+export { WebSocket as default } from "mock-socket";
 describe('EyePopSdk endpoint module auth and connect', () => {
     const server = new MockServer()
-    // let ws_server:WS|null = null
+    let ws_server:WS|null = null
     beforeAll(async () => {
         await server.start()
+        const wsUrl = `${server.getURL().toString().replace("https://", "wss://").replace("http://", "ws://")}data/events`
+        ws_server = new WS(wsUrl)
+        // ws_server.on("connection", (socket: MockWebSocket) => {
+        //
+        // })
     })
     afterAll(async () => {
-        // ws_server?.close()
+        ws_server?.close()
         await server.stop()
     })
 
@@ -51,8 +57,7 @@ describe('EyePopSdk endpoint module auth and connect', () => {
         const endpoint = EyePop.dataEndpoint({
             eyepopUrl: server.getURL().toString(),
             accountId: test_account_id,
-            auth: {secretKey: test_secret_key},
-            disableWs: true
+            auth: {secretKey: test_secret_key}
         })
         expect(endpoint).toBeDefined()
         try {
@@ -89,10 +94,7 @@ describe('EyePopSdk endpoint module auth and connect', () => {
             })
 
         const endpoint = EyePop.dataEndpoint({
-            eyepopUrl: server.getURL().toString(),
-            accountId: test_account_id,
-            auth: {secretKey: test_secret_key},
-            disableWs: true
+            eyepopUrl: server.getURL().toString(), accountId: test_account_id, auth: {secretKey: test_secret_key}
         })
         expect(endpoint).toBeDefined()
         try {
@@ -142,15 +144,12 @@ describe('EyePopSdk endpoint module auth and connect', () => {
                 ctx.body = JSON.stringify({base_url: `${server.getURL()}data/`})
             })
         const endpoint = EyePop.dataEndpoint({
-            eyepopUrl: server.getURL().toString(),
-            accountId: test_account_id,
-            auth: {secretKey: test_secret_key},
-            disableWs: true
+            eyepopUrl: server.getURL().toString(), accountId: test_account_id, auth: {secretKey: test_secret_key}
         })
         expect(endpoint).toBeDefined()
         try {
             await endpoint.connect()
-            expect(authenticationRoute).toHaveBeenCalledTimes(1)
+            expect(authenticationRoute).toHaveBeenCalledTimes(2)
             expect(dataConfigRoute).toHaveBeenCalledTimes(1)
         } finally {
             await endpoint.disconnect()
@@ -190,16 +189,13 @@ describe('EyePopSdk endpoint module auth and connect', () => {
             })
 
         const endpoint = EyePop.dataEndpoint({
-            eyepopUrl: server.getURL().toString(),
-            accountId: test_account_id,
-            auth: {secretKey: test_secret_key},
-            disableWs: true
+            eyepopUrl: server.getURL().toString(), accountId: test_account_id, auth: {secretKey: test_secret_key}
         })
         expect(endpoint).toBeDefined()
         try {
             expect(authenticationRoute).toHaveBeenCalledTimes(0)
             await endpoint.connect()
-            expect(authenticationRoute).toHaveBeenCalledTimes(2)
+            expect(authenticationRoute).toHaveBeenCalledTimes(3)
             expect(dataConfigRoute).toHaveBeenCalledTimes(2)
         } finally {
             await endpoint.disconnect()
@@ -228,10 +224,7 @@ describe('EyePopSdk endpoint module auth and connect', () => {
             })
 
         const endpoint1 = EyePop.dataEndpoint({
-            eyepopUrl: server.getURL().toString(),
-            accountId: test_account_id,
-            auth: {secretKey: test_secret_key},
-            disableWs: true
+            eyepopUrl: server.getURL().toString(), accountId: test_account_id, auth: {secretKey: test_secret_key}
         })
         expect(endpoint1).toBeDefined()
 
@@ -250,10 +243,7 @@ describe('EyePopSdk endpoint module auth and connect', () => {
         dataConfigRoute.mockClear()
 
         const endpoint2 = EyePop.dataEndpoint({
-            eyepopUrl: server.getURL().toString(),
-            accountId: test_account_id,
-            auth: {session: session},
-            disableWs: true
+            eyepopUrl: server.getURL().toString(), accountId: test_account_id, auth: {session: session}
         })
         expect(endpoint2).toBeDefined()
 
