f2e9bddd5fbddbc72133bfcc328cdcada6c10312
Edmund Dao <61922101+64blit@users.noreply.github.com>
2024-09-20 08:55:37 -0700

    [cleanup] moved vite into node folder

---
+++ examples/node/vite/src/main.ts
@@ -0,0 +1,125 @@
+import "eyepop";
+import "eyepop-render-2d";
+
+import { pino } from "pino";
+import image1Src from "example.jpg";
+import image2Src from "large_example.jpg";
+import { BoxType } from "../../../src/eyepop-render-2d/render-box";
+
+const logger = pino({ level: "info", name: "eyepop-example" });
+
+async function run(useDefault = true) {
+  const image = document.getElementById("image1") as HTMLImageElement;
+  const canvas = document.getElementById("canvas1") as HTMLCanvasElement;
+  const context = canvas.getContext("2d");
+
+  const image1 = document.getElementById("image2") as HTMLImageElement;
+  if (useDefault) {
+    image.src = image1Src;
+    image1.src = image2Src;
+  }
+  const canvas1 = document.getElementById("canvas2") as HTMLCanvasElement;
+  const context1 = canvas1.getContext("2d");
+
+  const imageBlob1 = await fetch(image.src).then((res) => res.blob());
+  const imageBlob2 = await fetch(image1.src).then((res) => res.blob());
+
+  const endpoint = await EyePop.endpoint({
+    auth: {
+      oAuth2: true,
+    },
+    popId: "ab3cb23c05c045a29ee6ea00c765f167",
+    logger: logger,
+  })
+    .onStateChanged((fromState: any, toState: any) => {
+      logger.info("Endpoint changed state %s -> %s", fromState, toState);
+    })
+    .connect();
+
+  try {
+    let results = await endpoint.process({ file: imageBlob1 });
+
+    for await (let result of await results) {
+      canvas.width = result.source_width;
+      canvas.height = result.source_height;
+      console.log(result);
+      let hasText = false;
+      for (let i = 0; i < result?.objects?.length; i++) {
+        let obj = result.objects[i];
+        if (obj.category === "text") {
+          hasText = true;
+          break;
+        }
+      }
+
+      context?.drawImage(image, 0, 0);
+      Render2d?.renderer(context, [
+        Render2d.renderPose(),
+        Render2d.renderFace(),
+        Render2d.renderHand(),
+        Render2d.renderBox({
+          showClass: !hasText,
+          showText: !hasText,
+          showConfidence: !hasText,
+          showTraceId: !hasText,
+          showNestedClasses: !hasText,
+          boxType: BoxType.Rich,
+        }),
+        Render2d.renderText(),
+      ]).draw(result);
+    }
+
+    results = await endpoint.process({ file: imageBlob2 });
+
+    for await (let result of await results) {
+      canvas1.width = result.source_width;
+      canvas1.height = result.source_height;
+      console.log(result);
+      let hasText = false;
+
+      for (let i = 0; i < result?.objects?.length; i++) {
+        let obj = result.objects[i];
+        if (obj.category === "text") {
+          hasText = true;
+          break;
+        }
+      }
+      context1?.drawImage(image1, 0, 0);
+      Render2d?.renderer(context1, [
+        Render2d.renderPose(),
+        Render2d.renderFace(),
+        Render2d.renderHand(),
+        Render2d.renderBox({
+          showClass: !hasText,
+          showText: !hasText,
+          showConfidence: !hasText,
+          showTraceId: !hasText,
+          showNestedClasses: !hasText,
+          boxType: BoxType.Simple,
+        }),
+        Render2d.renderText(),
+      ]).draw(result);
+    }
+  } finally {
+    await endpoint.disconnect();
+  }
+}
+
+document.addEventListener("DOMContentLoaded", async () => {
+  document.getElementById("url-input").addEventListener("input", async (event) => {
+    const imageUrl = (event.target as HTMLInputElement).value;
+    const image1 = document.getElementById("image1") as HTMLImageElement;
+    const newImage = new Image();
+    newImage.src = imageUrl;
+    image1.crossOrigin = "Anonymous";
+    newImage.crossOrigin = "Anonymous";
+
+    newImage.onload = () => {
+      image1.src = imageUrl;
+      console.log(image1.src);
+      run(false);
+    };
+  });
+
+  run();
+});
---
+++ examples/node/vite/src/vite.config.js
@@ -0,0 +1,17 @@
+
+import { defineConfig } from 'vite'
+import path from 'path'
+
+export default defineConfig({
+    server: {
+        port: 8000,
+    },
+    resolve: {
+        alias: {
+            'eyepop': path.resolve(__dirname, '../../../src/eyepop/dist/eyepop.min.js'),
+            'eyepop-render-2d': path.resolve(__dirname, '../../../src/eyepop-render-2d/dist/eyepop.render2d.min.js'),
+            'example.jpg': path.resolve(__dirname, '../../example.jpg'),
+            'large_example.jpg': path.resolve(__dirname, '../../large_example.jpg'),
+        }
+    },
+})
