5f98c6452f46d654254e6590ee0b7e966460d2e9
Edmund Dao <61922101+64blit@users.noreply.github.com>
2024-09-20 09:08:23 -0700

    Update webpack.config.js

--- src/eyepop/webpack.config.js
+++ src/eyepop/webpack.config.js
@@ -1,18 +1,25 @@
 const path = require('path');
 const NodePolyfillPlugin = require("node-polyfill-webpack-plugin");
+const webpack = require('webpack');
 
-  module.exports = {
-    entry: path.resolve(__dirname, 'dist', 'eyepop.index.js'),
-    output: {
-      path: path.resolve(__dirname, 'dist'),
-      filename: 'eyepop.min.js',
-      library: {
-        name: "EyePop",
-        type: "umd",
-        export: ['default']
-      },
+module.exports = {
+  entry: path.resolve(__dirname, 'dist', 'eyepop.index.js'),
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'eyepop.min.js',
+    library: {
+      name: "EyePop",
+      type: "umd",
+      export: [ 'default' ]
     },
-    plugins: [
-        new NodePolyfillPlugin()
-    ]
-  };
+  },
+  plugins: [
+    new NodePolyfillPlugin(),
+    new webpack.ProvidePlugin({
+      Buffer: [ 'buffer', 'Buffer' ],
+    }),
+    new webpack.ProvidePlugin({
+      process: 'process/browser',
+    })
+  ]
+};
