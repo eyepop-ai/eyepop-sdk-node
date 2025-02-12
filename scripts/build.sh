0be0183d37132c2272d08ea9cb29ffe8447f83cc
Edmund Dao <61922101+64blit@users.noreply.github.com>
2025-02-12 15:17:33 -0800

    add build script

---
+++ scripts/build.sh
@@ -0,0 +1,32 @@
+# /usr/bin/bash
+
+# Set the current directory of shell file into a variable
+current_dir=$(dirname "$0")
+cd "$current_dir" 
+
+echo '##############################'
+echo '##'
+echo '##'
+echo '##'
+echo "##  Building eyepop"
+echo '##'
+echo '##'
+echo '##'
+echo '##############################'
+cd ../src/eyepop 
+npm install > /dev/null
+npm run build 
+
+
+echo '##############################'
+echo '##'
+echo '##'
+echo '##'
+echo "##  Building eyepop-render-2d"
+echo '##'
+echo '##'
+echo '##'
+echo '##############################'
+cd ../eyepop-render-2d
+npm install > /dev/null
+npm run build 
