9490cace3d04cbec41d80aa037c292fbe517bd4f
Torsten Schulz <torsten@eyepop.ai>
2025-03-04 15:11:17 -0800

    [cleanup] VideoMode align names and example for VideoMode.STREAM (#107)

--- examples/node/upload_video.ts
+++ examples/node/upload_video.ts
@@ -1,13 +1,16 @@
-import { EyePop } from '../../src/eyepop'
+import { EyePop, VideoMode } from '@eyepop.ai/eyepop'
 import process from 'process'
 import { pino } from 'pino'
 
 const logger = pino({ level: 'debug', name: 'eyepop-example' })
 
-async function upload_video(video_path: string, seconds: number) {
+async function upload_video(video_path: string, is_streaming: boolean, seconds: number) {
     const endpoint = await EyePop.workerEndpoint({ logger: logger }).connect()
     try {
-        const results = await endpoint.process({ path: video_path })
+        const results = await endpoint.process({
+            path: video_path,
+            videoMode: is_streaming? VideoMode.STREAM: undefined
+        })
         for await (let result of results) {
             // @ts-ignore
             if (result['seconds'] >= seconds) {
@@ -23,9 +26,10 @@ async function upload_video(video_path: string, seconds: number) {
 ;(async () => {
     try {
         const video_file = process.argv[2]
+        const is_streaming = process.argv.length > 3 && process.argv[3].startsWith('s')
         const seconds = 60.0
         console.log(`using: ${video_file} for ${seconds} seconds`)
-        await upload_video(video_file, seconds)
+        await upload_video(video_file, is_streaming, seconds)
     } catch (e) {
         console.error(e)
     }
